aa0307dc6c772f51c2513c3b551c616277141c12
Brady Snelson <bsnelson@umd.edu>
2021-03-30 17:00:29 -0400

    Django initial models

--- EdgarScraper/company/models.py
+++ EdgarScraper/company/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField()
+    cik = models.CharField()
+
+    def __str__(self):
+        return self.name
