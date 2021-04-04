7dc8ad2786ef0f9ac05a78d288732e537b87b51f
ggarcz1 <ggarcz1@terpmail.umd.edu>
2021-04-04 19:50:44 -0400

    dict and databases updates to company.py

--- py-edgar-lite/edgar/company.py
+++ py-edgar-lite/edgar/company.py
@@ -1,9 +1,20 @@
+import re
 from typing import List
 import requests
 from lxml import html
 import lxml
 
 BASE_URL = "https://www.sec.gov"
+
+
+def get_10k_year(url):
+    url = url.split('/')
+    return '20' + url[7][10:12]
+
+
+def get_10Q_year_seq_number(url):
+    url = url.split('/')
+    return ['20' + url[7][10:12], url[7][12:]]
 
 
 class Company:
@@ -15,6 +26,9 @@ class Company:
         self.timeout = timeout
         self._document_urls = []
         self._excel_urls = []
+        # [url, year]
+        # [url, year, quarterNumber]
+        self.form_type = {'10-K': [], '10-Q': []}
 
     @property
     def document_urls(self):
@@ -34,16 +48,32 @@ class Company:
         page = self._get(url)
         return html.fromstring(page.content)
 
-    def get_company_excel_reports_from_10K(self) -> List[str]:
+    def get_company_excel_reports_from(self, report_type) -> List[str]:
         """
-        Retrieve the company's excel format 10-K report
+        Retrieve the company's excel format 10-K or 10-Q report
         """
-        page = self.get_all_filings(filing_type="10-K")
+        # determine type of request
+        regex = re.search('10-K|10-Q', report_type)
+        if not regex:
+            return None
+
+        page = self.get_all_filings(filing_type=report_type)
 
         self._document_urls = [BASE_URL + elem.attrib["href"]
                                for elem in page.xpath("//*[@id='documentsbutton']") if elem.attrib.get("href")]
-        self._excel_urls = ['/'.join(elem.split('/')[:-1] + ["Financial_Report.xlsx"])
-                            for elem in self._document_urls]
+
+        # add to the form_type dict
+        for elem in self._document_urls:
+            new_url = '/'.join(elem.split('/')[:-1] + ["Financial_Report.xlsx"])
+            if report_type == '10-K':
+                new_url = [new_url, get_10k_year(new_url)]
+            else:
+                year_seq = get_10Q_year_seq_number(new_url)
+                # figure out how to put them into quarters
+                new_url = (new_url, year_seq[0], year_seq[1])
+
+            self.form_type[report_type].append(new_url)
+
         return self._excel_urls
 
     def download_file(self, url, cid) -> bool:
@@ -51,8 +81,29 @@ class Company:
             return False
 
         req = requests.get(url, allow_redirects=True)
-        # may need to append .xlsx to end of file name
         file = open('report_' + self.name + '.xlsx', 'wb')
         file.write(req.content)
         file.close()
         return True
+
+    # return all 10-K and 10-Q's
+    def get_form_types(self):
+        return self.form_type
+
+    # return 10-K
+    def get_10k_year(self, year):
+        tenK_lst = self.form_type['10-K']
+        for idx in tenK_lst:
+            if idx[1] == year:
+                return idx[0]
+        return None
+
+    # return 10-Q
+    def get_10Q_year(self, year, quarter):
+        tenK_lst = self.form_type['10-K']
+        for idx in tenK_lst:
+            if idx[1] == year:
+                if idx[2] == quarter:
+                    return idx[0]
+        return None
+
