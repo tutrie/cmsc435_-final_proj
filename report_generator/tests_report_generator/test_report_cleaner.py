e55dd303ebb8a669264e83e525b6c3b9a90bf3ce
tutrie <40398781+tutrie@users.noreply.github.com>
2021-04-14 19:42:37 -0400

    fixed dataframe index

--- report_generator/tests_report_generator/test_report_cleaner.py
+++ report_generator/tests_report_generator/test_report_cleaner.py
@@ -1,20 +1,20 @@
 import unittest
-from report_generator.utils.report_cleaner import excelcleaner
+from report_generator.utils.report_cleaner import cleaner
 
 file_path = "ReportGenerator/excel_reports/10-K-20.xlsx"
 
 
 class MyTestCase(unittest.TestCase):
     def test_CleanedExcelReport_notes(self):
-        test_object = excelcleaner.CleanedExcelReport(file_path)
+        test_object = cleaner.CleanedExcelReport(file_path)
         self.assertEqual(8, len(test_object.notes.keys()))
 
     def test_CleanedExcelReport_original(self):
         # 97 sheets in OG 10-k-20 report
-        test_object = excelcleaner.CleanedExcelReport(file_path)
+        test_object = cleaner.CleanedExcelReport(file_path)
         self.assertEqual(97, len(test_object.excel_report.sheetnames))
 
     def test_CleanedExcelReport_cleaned(self):
         # 97 sheets in OG 10-k-20 report
-        test_object = excelcleaner.CleanedExcelReport(file_path)
+        test_object = cleaner.CleanedExcelReport(file_path)
         self.assertEqual(8, len(test_object.cleaned_excel_report.sheetnames))
--- report_generator/utils/report_cleaner/excelToPandasToJson.py
+++ report_generator/utils/report_cleaner/excelToPandasToJson.py
@@ -1,8 +1,10 @@
-from .excelcleaner import CleanedExcelReport
 from report_generator.utils.convert_objects.object_conversions import *
+from .cleaner import *
+import openpyxl as pyxl
 
 
 class ConvertCleanSave:
     def __init__(self, file: str):
-        self.excel_report = CleanedExcelReport(file)
-        self.pandas_dict = workbook_to_dataframes_dict(self.excel_report.cleaned_excel_report)
+        self.excel_report = pyxl.load_workbook(file)  # the original workbook in case needed in future
+        self.cleaned_excel_report, self.notes = ten_k_excel_cleaning(pyxl.load_workbook(file))  # pyxl Workbook object
+        self.pandas_dict = ten_k_workbook_to_dataframes_dict(self.cleaned_excel_report, self.notes)
