33b9964cb78fe0949d494cee95718aef1e50a2e4
ggarcz1 <ggarcz1@terpmail.umd.edu>
2021-04-18 11:22:13 -0400

    update tests

--- api_comms/README.md
+++ api_comms/README.md
@@ -2,3 +2,7 @@ To run the server:
 1. cd into api_comms\flask_app
 2. run python3 -m flask run
 3. visit http://127.0.0.1:5000/
+
+
+To run tests: 
+1. do the above to make sure the server is running or else the tests will not work as they are supposed to.
--- api_comms/flask_app/test_flask_app.py
+++ api_comms/flask_app/test_flask_app.py
@@ -1,22 +1,32 @@
-import pytest
+import json
+import requests
+
 BASE_URL = 'http://127.0.0.1:5000/'
 
 
-def test_main(client):
-    resp = client.get("/")
-    assert resp.status_code == 200
-
-
-def test_login(client):
-    response = client.get('/login')
+def test_main():
+    response = requests.get(BASE_URL)
+    assert response.status_code == 200
+    response = requests.post(BASE_URL)
+    assert response.status_code != 200
+    response = requests.get(BASE_URL)
     assert response.status_code == 200
 
 
-def test_register(client):
-    response = client.get('/register')
+# not working bc cant send it to the django server
+def test_login():
+    url = BASE_URL + 'login'
+    package = {'username': 'admin',
+               'password': 'admin'}
+
+    response = requests.post(url, data=json.dumps(package))
     assert response.status_code == 200
 
 
-def test_logout(client):
-    response = client.get('/logout')
+def test_logout():
+    url = BASE_URL + 'logout'
+    response = requests.get(url)
     assert response.status_code == 200
+
+# def test_register(client):
+
