53daf0edda44d4786d71f069c361cf3e771235a0
ggarcz1 <ggarcz1@terpmail.umd.edu>
2021-04-19 09:45:06 -0400

    Update app.py

--- api_comms/flask_app/app.py
+++ api_comms/flask_app/app.py
@@ -24,12 +24,8 @@ def register():
 
         response = requests.post('http://18.217.8.244:8000/api/users/create_user/',
                                  data=data, timeout=5)
-        print(response.status_code)
-        if response.status_code != 201 or response.status_code != 200:
-            return render_template('register.html', title='Register')
-        elif response.status_code == 201 or response.status_code == 200:
-            return render_template('login.html', title='login')
-            # return redirect(url_for('login'))
+        if response.status_code == 201 or response.status_code == 200:
+            return redirect(url_for('login'))
 
     return render_template('register.html', title='Register')
 
@@ -37,8 +33,6 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        # print(len(request.data) == 0)
-        # get input from the form
         if len(request.data) == 0:
             data = {"username": request.form['username'],
                     "password": request.form['password']}
@@ -48,13 +42,11 @@ def login():
                                 data=data, timeout=5)
 
         print(response.status_code)
-        if response.status_code != 200:
-            return render_template('login.html', title='login')
-        elif response.status_code == 200:
+        if response.status_code == 200:
             response = json.loads(response)
             # send data to the account display html
-            # return redirect(url_for('account'))
-            return render_template('account.html', name=request.data['username'], data=response)
+            return redirect(url_for('account'))
+            # return render_template('account.html', name=request.data['username'], data=response)
 
     return render_template('login.html', title='login')
 
