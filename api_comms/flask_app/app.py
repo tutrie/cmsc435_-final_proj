3dadb44d567de25b48fe77e1275e796161f960a5
ggarcz1 <ggarcz1@terpmail.umd.edu>
2021-04-19 09:13:24 -0400

    update login and register

--- api_comms/flask_app/app.py
+++ api_comms/flask_app/app.py
@@ -14,46 +14,55 @@ def main_page():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    print(request.method)
+    print(request.url)
+    print(request.data)
     if request.method == 'POST':
-        username = request.form['username']
-        password1 = request.form['password1']
-        password2 = request.form['password2']
+        # print(len(request.data) == 0)
+        if len(request.data) == 0:
+            data = {"username": request.form['username'],
+                    "password": request.form['password'],
+                    "email": request.form['email']}
+            print(data)
+        else:
+            data = request.data
 
-        if password1 != password2:
-            error = "Passwords do not match!"
-            return render_template('register.html', error=error, title='Register')
+        response = requests.post('http://18.217.8.244:8000/api/users/create_user/',
+                                 data=data, timeout=5)
 
-        # send to the server and make sure
-        if username is not None and password1 is not None and password2 is not None:
-            data = {username: username,
-                    password1: password1}
-            response = requests.get('http://localhost:8000/api/users/create_user',
-                                    data=json.dumps(data), timeout=5)
-            if response.status == 403:
-                return render_template('register.html', title='Register')
-            else:
-                return redirect(url_for('login'))
+        if response.status_code != 200:
+            return render_template('register.html', title='Register')
+        elif response.status_code == 200:
+            return redirect(url_for('login'))
 
     return render_template('register.html', title='Register')
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    print(request.method)
+    print(request.url)
+    print(request.data)
     if request.method == "POST":
-        username = request.form['username']
-        password = request.form['password']
+        # print(len(request.data) == 0)
+        # get input from the form
+        if len(request.data) == 0:
+            data = {"username": request.form['username'],
+                    "password": request.form['password']}
+            print(data)
+        else:
+            data = request.data
+        response = requests.get('http://18.217.8.244:8000/api/generated-reports/',
+                                data=data, timeout=5)
 
-        if username is not None and password is not None:
-            data = {username: username,
-                    password: password}
-            response = requests.get('http://localhost:8000/api/generated_reports',
-                                    data=json.dumps(data), timeout=5)
-            if response.status == 403:
-                return render_template('login.html', title='login')
-            else:
-                response = json.loads(response)
-                # send data to the account display html
-                return render_template('account.html', name=username, data=response)
+        print(response.status_code)
+        if response.status_code != 200:
+            return render_template('login.html', title='login')
+        elif response.status_code == 200:
+            response = json.loads(response)
+            # send data to the account display html
+            # return redirect(url_for('account'))
+            return render_template('account.html', name=request.data['username'], data=response)
 
     return render_template('login.html', title='login')
 
--- api_comms/flask_app/test_flask_app.py
+++ api_comms/flask_app/test_flask_app.py
@@ -13,22 +13,21 @@ def test_main():
     assert response.status_code == 200
 
 
-# not working bc cant send it to the django server, so a != 200  code
 def test_login():
     url = BASE_URL + 'login'
     package = {'username': 'admin',
                'password': 'admin'}
 
-    response = requests.post(url, data=json.dumps(package))
+    response = requests.get(url, data=json.dumps(package))
     assert response.status_code == 200
 
 
 # not working bc cant send it to the django server, so a != 200  code
 def test_register():
     url = BASE_URL + 'register'
-    package = {'username': 'bob',
-               'password1': 'bob',
-               'password2': 'bob'}
+    package = {"username": "q",
+               "password1": "q",
+               "email": "q@gmail.com"}
 
     response = requests.post(url, data=json.dumps(package))
     assert response.status_code == 200
@@ -40,10 +39,21 @@ def test_logout():
     assert response.status_code == 200
 
 
-# def test_login():
-#     url = 'https://www.geeksforgeeks.org/render-html-forms-get-post-in-django/'
-#     package = {'username': 'admin',
-#                'password': 'admin'}
+def register_failure_duplicate_user():
+    url = BASE_URL + 'register'
+    package = {'username': 'admin',
+               'password': 'admin',
+               "email": "q@gmail.com"}
+
+    response = requests.get(url, data=json.dumps(package))
+    assert b'A user with that' in response
+
+# def login_failure_no_user():
+#     url = BASE_URL + 'logout'
+#     package = {'username': 'asdfadsfdsaf',
+#                'password': 'dfsafsdfsda'}
+#     response = requests.get(url, data=json.dumps(package))
+#     assert response
 #
-#     response = requests.post(url, data=json.dumps(package))
-#     assert response.status_code == 200
+#
+
