92102115b306fd4b7806c881ec51aa15db198ae0
ggarcz1 <ggarcz1@terpmail.umd.edu>
2021-04-19 13:01:19 -0400

    Update test_flask_app.py

--- api_comms/flask_app/test_flask_app.py
+++ api_comms/flask_app/test_flask_app.py
@@ -4,13 +4,21 @@ import requests
 BASE_URL = 'http://127.0.0.1:5000/'
 
 
+# send a request with the right json body
+
+
+# make sure pages load correctly
 def test_main():
     response = requests.get(BASE_URL)
     assert response.status_code == 200
-    response = requests.post(BASE_URL)
-    assert response.status_code != 200
-    response = requests.get(BASE_URL)
+    response = requests.get(BASE_URL + 'login')
     assert response.status_code == 200
+    response = requests.get(BASE_URL + 'register')
+    assert response.status_code == 200
+    response = requests.get(BASE_URL + 'logout')
+    assert response.status_code == 200
+    response = requests.get(BASE_URL + 'asdasas')
+    assert response.status_code != 200
 
 
 def test_login():
@@ -22,7 +30,6 @@ def test_login():
     assert response.status_code == 200
 
 
-# not working bc cant send it to the django server, so a != 200  code
 def test_register():
     url = BASE_URL + 'register'
     package = {"username": "q",
@@ -37,23 +44,3 @@ def test_logout():
     url = BASE_URL + 'logout'
     response = requests.get(url)
     assert response.status_code == 200
-
-
-def register_failure_duplicate_user():
-    url = BASE_URL + 'register'
-    package = {'username': 'admin',
-               'password': 'admin',
-               "email": "q@gmail.com"}
-
-    response = requests.get(url, data=json.dumps(package))
-    assert b'A user with that' in response
-
-# def login_failure_no_user():
-#     url = BASE_URL + 'logout'
-#     package = {'username': 'asdfadsfdsaf',
-#                'password': 'dfsafsdfsda'}
-#     response = requests.get(url, data=json.dumps(package))
-#     assert response
-#
-#
-
