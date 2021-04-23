d07f609225219c14cbe345bdedbde7b3b6ea2465
Siyao Li <siyaoli@terpmail.umd.edu>
2021-04-23 19:40:11 -0400

    Added docstring to the app.py file

--- api_comms/flask_app/app.py
+++ api_comms/flask_app/app.py
@@ -2,6 +2,16 @@ import os
 import requests
 from flask import Flask, session, redirect, url_for, request, render_template
 
+"""
+A file that defines the routes for the frontend Flask application.
+
+This file configures the routes and render templates for the UI. Calls the Django API for data retrieval and 
+stores the user's information in the session 
+
+Fields:
+    app: The Flask app instance
+    UI_PORT: The port that this Flask app will be ran on.
+"""
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xe0\x8d?8z\xdd\x87i}\xfc\xaa\x91\x8f\n1\x1a\xe4\xb3\xa7\xbd5\xf8\x96\xdd'
 
@@ -10,11 +20,25 @@ UI_PORT = os.getenv('UI_PORT')
 
 @app.route('/')
 def main_page():
+    """
+    A function called when there's a GET request to index route received.
+
+    Returns:
+        Renders mainpage.html template which shows the homepage of the app.
+    """
     return render_template('mainpage.html', title='Main Page')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    A function called when there's either a GET or POST request to register route received.
+
+    Returns:
+        For GET request, renders register.html template which shows the register page of the app.
+        For POST request, renders login.html template which shows the login page of the app after sending a POST
+        request to the registration API.
+    """
     if request.method == 'POST':
         data = request.data
 
@@ -28,6 +52,13 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    """
+    A function called when there's either a GET or POST request to login route received.
+    For POST request, the username and password will be stored into session of the app.
+
+    Returns:
+        Renders login.html template which shows the login page of the app with the username stored in the session.
+    """
     if request.method == 'POST':
         session['username'] = request.form['username']
         session['password'] = request.form['password']
@@ -37,17 +68,28 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """
+    A function called when there's a GET request to login route received.
+    Removes the username in the flask session.
+
+    Returns:
+        Renders logout.html template which shows the logout page of the app.
+    """
     session.pop('username', None)
     return render_template('logout.html', title='Logout')
 
 
-@app.route('/account')
-def account():
-    return render_template('account.html', title='Account')
-
-
 @app.route('/raw_report', methods=['GET', 'POST'])
 def raw_report():
+    """
+    A function called when there's either a GET or POST request to raw_report route received.
+    For POST request, send request to raw-reports API to retrieve the report information, and iterate over the
+    reports to find the matched report. Add the url to the parameter of the rendered template.
+
+    Returns:
+        Renders raw_report.html template which shows the raw_report page of the app with information of the requested
+        reports and the current username.
+    """
     if request.method == 'POST':
         data = request.form
         response_raw = requests.get('http://18.217.8.244:8000/api/raw-reports/', timeout=15)
@@ -57,28 +99,38 @@ def raw_report():
             reports = response_raw.json()
             for report in reports:
                 company = report['company']
-                match = company['name'] == data['name'] and \
-                        company['cik'] == data['cik'] and \
-                        report['report_date'] == data['report_date']
+                match = company['name'] == data['name'] and company['cik'] == data['cik'] and \
+                    report['report_date'] == data['report_date']
                 if match:
                     excel_url = report['excel_url']
                     break
         print(excel_url)
-        return render_template('raw_report.html', title='Raw Report', username=session.get('username'), excel_url=excel_url)
-    
+        return render_template('raw_report.html', title='Raw Report',
+                               username=session.get('username'), excel_url=excel_url)
+
     return render_template('raw_report.html', title='Raw Report', username=session.get('username'))
 
 
 @app.route('/generated_report')
 def generated_report():
+    """
+    A function called when there's either a GET request to raw_report route received.
+    It sends a request to the API to retrieve the generated report information, and add the json response
+    to the parameter of the rendered template.
+
+    Returns:
+        Renders generated_report.html template which shows the generated_report page of the app
+        with information of the current user's generated reports and the username.
+    """
     username = session.get('username')
-    generated_report = None
+    report = None
     if username:
         response_generated = requests.get('http://18.217.8.244:8000/api/generated-reports/',
-                                    auth=(session.get('username'), session.get('password')), timeout=15)
+                                          auth=(session.get('username'), session.get('password')), timeout=15)
         if response_generated.status_code == 200:
-            generated_report = response_generated.json()
-    return render_template('generated_report.html', title='Generated Report', generated_report=generated_report, username=username)
+            report = response_generated.json()
+    return render_template('generated_report.html', title='Generated Report',
+                           generated_report=report, username=username)
 
 
 if __name__ == '__main__':
