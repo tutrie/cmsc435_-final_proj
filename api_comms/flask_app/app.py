cbb72ae269a2d2b23666c1ad864f5573bbd8aa79
Patrick Donnelly <pdonn3@umd.edu>
2021-04-28 16:50:24 -0400

    updated server error status code

--- api_comms/flask_app/app.py
+++ api_comms/flask_app/app.py
@@ -172,7 +172,7 @@ def analysis(report_id: str):
             return render_template('analysis.html', title='Report Analysis',
                                    report=response.json(),
                                    username=username), 200
-        
+
         if response.status_code == 404:
             return render_template('not_found.html', title='Report Not '
                                                            'Found'), 404
@@ -180,7 +180,8 @@ def analysis(report_id: str):
         if response.status_code == 403:
             return render_template('forbidden.html', title='Forbidden'), 403
 
-        return render_template('server_error.html', title='Server Error'), 500
+        return render_template('server_error.html', title='Server Error'), \
+               response.status_code
 
     # user not logged in
     return render_template('login.html', title='Login',
