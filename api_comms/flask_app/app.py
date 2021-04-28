f3caf792245d292686c155e7b21cf50dfbb3e176
Patrick Donnelly <pdonn3@umd.edu>
2021-04-28 16:39:27 -0400

    updated analysis endpoint

--- api_comms/flask_app/app.py
+++ api_comms/flask_app/app.py
@@ -172,7 +172,6 @@ def analysis(report_id: str):
             return render_template('analysis.html', title='Report Analysis',
                                    report=response.json(),
                                    username=username), 200
-
         if response.status_code == 404:
             return render_template('not_found.html', title='Report Not '
                                                            'Found'), 404
