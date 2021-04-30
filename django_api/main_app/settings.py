34b78e9a9659d000c38652fcdfd14fe8dcfd6d70
Patrick Donnelly <pdonn3@umd.edu>
2021-04-29 23:37:50 -0400

    updated django settings for postgres docker connection

--- django_api/main_app/settings.py
+++ django_api/main_app/settings.py
@@ -82,24 +82,13 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        # Add the docker environment SQL_ENGINE variable or for local development use sqlite3 engine
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-
-        # Add the docker environment SQL_DATABASE variable or use the local sqlite database soruce
-        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
-
-        # Add the docker SQL_USER environment variable or on need password for sqlite3
-        "USER": os.environ.get("SQL_USER", ""),
-
-        # Add the docker SQL_PASSWORD environment variable or on need password for sqlite3
-        "PASSWORD": os.environ.get("SQL_PASSWORD", ""),
-
-        # Add the docker SQL_HOST environment variable or on need host for sqlite3
-        "HOST": os.environ.get("SQL_HOST", ""),
-
-        # Add the docker SQL_HOST environment variable or on need port for sqlite3
-        "PORT": os.environ.get("SQL_PORT", ""),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres-db',
+        'PORT': '5432'
     }
 }
 
